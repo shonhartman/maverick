b62bfb0c221eb33bea21019fbd429d93d35c1afe
shonhartman <shaun@bighuman.com>
2022-11-13 14:37:24 -0500

    jet plane

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas
+          shadows
+          camera={ {
+              fov: 45,
+              near: 0.1,
+              far: 200,
+              position: [ - 4, 3, 6 ]
+          } }
+      >
+          <Experience />
+      </Canvas>
     </div>
   );
 }
